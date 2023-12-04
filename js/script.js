0dd58bfad6e2e61a594804935fe65fb667f6bc09
ClaudioScordari <scordari22@gmail.com>
2023-12-04 18:23:32 +0100

    Esercizio terminato

--- js/script.js
+++ js/script.js
@@ -12,27 +12,53 @@
 
 // Prezzo totale del viaggio - ?
 
-// età del passeggero
-const eta_passeggero = prompt('età del passeggero:');
+// dichiarazione del bottone in html
+const myButton = document.querySelector('.my-button');
 
-// km che vuole percorrere
-const km_passeggero = prompt('quanti km vuoi viaggiare?');
+// assegniamo al bottone la mia funzione
+myButton.addEventListener('click', function () {
 
-// euro per km
-const costo_1km = 0.21;
+    // nome e cognome dell'user
+    const userName = document.querySelector('.name-lastname');
 
-// prezzo del viaggio senza sconto
-let prezzo_viaggio = km_passeggero * costo_1km;
+    // età del passeggero
+    let userAge = document.querySelector('.age');
+    userAgeValue = parseInt(userAge.value);
 
-// costo del biglietto per i minorenni
-if (eta_passeggero < 18) {
-    prezzo_viaggio = prezzo_viaggio - (prezzo_viaggio * 0.2);
-}
+    // km che vuole percorrere
+    let userKm = document.querySelector('.km');
+    userKmValue = parseInt(userKm.value);
 
-// costo del biglietto per gli over 65
-if (eta_passeggero >= 65) {
-    prezzo_viaggio = prezzo_viaggio - (prezzo_viaggio * 0.4);
-}
+    // euro per km
+    const costo_1km = 0.21;
 
-// prezzo finale - stampato in html
-document.querySelector('.prezzo_biglietto').innerHTML = prezzo_viaggio.toFixed(2);
+    // prezzo del viaggio senza sconto
+    let prezzo_viaggio = userKmValue * costo_1km;
+
+    // costo del biglietto per i minorenni
+    if (userAgeValue < 18) {
+        prezzo_viaggio -= (prezzo_viaggio * 0.2);
+    }
+
+    // costo del biglietto per gli over 65
+    if (userAgeValue >= 65) {
+        prezzo_viaggio -= (prezzo_viaggio * 0.4);
+    }
+
+    document.querySelector('.final-price').innerHTML = prezzo_viaggio.toFixed(2);
+});
+
+// CONTINUAZIONE ESERCIZIO  
+
+/*
+
+    MILESTONE 1:
+    Iniziamo implementando il programma senza alcuna estetica: usando esclusivamente due input e un bottone (non stilizzati), realizziamo le specifiche scritte sopra. La risposta finale (o output) sarà da scrivere in console.
+    
+    MILESTONE 2:
+    Solo una volta che il milestone 1 sarà completo e funzionante allora realizzeremo un form in pagina in cui l'utente potrà inserire i dati e visualizzare il calcolo finale con il prezzo.
+    Il recap dei dati e l'output del prezzo finale, andranno quindi stampati in pagina (il prezzo dovrà essere formattato con massimo due decimali, per indicare i centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+    Nota:
+    Se non vi sentite particolarmente creativi, questa potrebbe essere un'implementazione da seguire per il secondo milestone. Potete scegliere di implementare una soluzione completamente diversa oppure simile, ma in ogni caso cercate di farla vostra. 
+
+*/
